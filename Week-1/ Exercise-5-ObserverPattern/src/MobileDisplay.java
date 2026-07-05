63bdfc5ad229f2fdbadffdcb2d9efcc56b73e53d
Tejaswini1904 <tejaec084@rmkcet.ac.in>
2026-07-05 19:13:02 +0530

    Create MobileDisplay.java

---
+++ Week-1/ Exercise-5-ObserverPattern/src/MobileDisplay.java
@@ -0,0 +1,7 @@
+public class MobileDisplay implements Observer {
+
+    @Override
+    public void update(float temperature) {
+        System.out.println("Mobile Display: Temperature updated to " + temperature + "°C");
+    }
+}
