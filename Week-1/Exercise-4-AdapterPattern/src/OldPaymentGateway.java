653861adbede31b0517e661728a997f7ce244703
Tejaswini1904 <tejaec084@rmkcet.ac.in>
2026-07-05 19:03:24 +0530

    Create OldPaymentGateway.java

---
+++ Week-1/Exercise-4-AdapterPattern/src/OldPaymentGateway.java
@@ -0,0 +1,6 @@
+public class OldPaymentGateway {
+
+    public void makePayment(double amount) {
+        System.out.println("Payment of ₹" + amount + " processed using Old Payment Gateway.");
+    }
+}
