7d62e8aabbf0c6ce5ffc3fd55152cf2e4b63b64f
Tejaswini1904 <tejaec084@rmkcet.ac.in>
2026-07-05 18:28:50 +0530

    Create ComputerBuilder.java

---
+++ Week-1/Exercise-3-BuilderPattern/src/ComputerBuilder.java
@@ -0,0 +1,37 @@
+public class ComputerBuilder {
+
+    private String cpu;
+    private String ram;
+    private String storage;
+    private boolean graphicsCard;
+    private boolean wifi;
+
+    public ComputerBuilder setCPU(String cpu) {
+        this.cpu = cpu;
+        return this;
+    }
+
+    public ComputerBuilder setRAM(String ram) {
+        this.ram = ram;
+        return this;
+    }
+
+    public ComputerBuilder setStorage(String storage) {
+        this.storage = storage;
+        return this;
+    }
+
+    public ComputerBuilder setGraphicsCard(boolean graphicsCard) {
+        this.graphicsCard = graphicsCard;
+        return this;
+    }
+
+    public ComputerBuilder setWifi(boolean wifi) {
+        this.wifi = wifi;
+        return this;
+    }
+
+    public Computer build() {
+        return new Computer(cpu, ram, storage, graphicsCard, wifi);
+    }
+}
